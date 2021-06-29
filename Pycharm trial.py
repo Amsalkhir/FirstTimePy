fadd8b02e661648e1d7901ac79a5090b9e09f33d
Abderrahman <abderrahman.a@outlook.com>
2021-06-29 10:07:59 +0200

    We in this

---
+++ Pycharm trial.py
@@ -0,0 +1,11 @@
+name = "AbderrahMAn amsaLKhir"
+coursename = "data AnalyTIcs"
+candidates = 10
+
+print(f"""Hi my name is {name.title()}! 
+I am participating in the course {coursename.lower()}. 
+There are {candidates} in taking this course""")
+
+
+
+print(name.count(""))
